bd54cee07eb814038d52e0bc5736dec299c37813
aser161 <aser161@yandex.ru>
2022-10-19 16:22:40 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        // Задача 1
+
+         // Задача 1
         System.out.println("Задача 1");
         var dog = 8;
         System.out.println(dog);
